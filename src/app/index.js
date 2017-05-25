84e0e829580bbd83498a5e59b0e59ac8836c0830
le_chiffre <le.chiffreua1gmail.com>
2017-05-25 15:14:31 +0300

    first nested component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,26 @@
-console.log(1);
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Home } from './components/Home';
+import { Header } from './components/Header';
+
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10">
+                        <Home/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<AppComponent/>, window.document.getElementById('app'));
